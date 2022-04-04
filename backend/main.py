ff105f0c4f23c8b783a3d0b0e303c408daf9d3a7
Tan Teck Jun <tanteckjun@gmail.com>
2022-04-04 23:34:21 +0800

    Added logic for TTN subscribing

--- backend/main.py
+++ backend/main.py
@@ -3,17 +3,18 @@ from flask_mqtt import Mqtt
 from flask import jsonify
 from flask_cors import CORS
 import datetime
+import json
 
 app = Flask(__name__)
-app.config['MQTT_BROKER_URL'] = '0.0.0.0'
-app.config['MQTT_BROKER_PORT'] = 1883
-app.config['MQTT_REFRESH_TIME'] = 1.0  # refresh time in seconds
+# app.config['MQTT_BROKER_URL'] = '0.0.0.0'
+# app.config['MQTT_BROKER_PORT'] = 1883
+# app.config['MQTT_REFRESH_TIME'] = 1.0  # refresh time in seconds
 
 ### Configuration for the things network
-#app.config['MQTT_BROKER_URL'] = 'au1.cloud.thethings.network'
-#app.config['MQTT_BROKER_PORT'] = 1883
-#app.config['MQTT_USERNAME'] = 'p1-05-mall-tracker@ttn'
-#app.config['MQTT_PASSWORD'] = 'NNSXS.R6IEXJFZ53YFNSXIFDW33GWGB7QSUZ7F23TKWFQ.YSYYI4ESMDEONNMACH4OEMK2OWQUG76IRX2IQQHP2VYNSI7OHRAQ'
+app.config['MQTT_BROKER_URL'] = 'au1.cloud.thethings.network'
+app.config['MQTT_BROKER_PORT'] = 1883
+app.config['MQTT_USERNAME'] = 'p1-05-mall-tracker@ttn'
+app.config['MQTT_PASSWORD'] = 'NNSXS.R6IEXJFZ53YFNSXIFDW33GWGB7QSUZ7F23TKWFQ.YSYYI4ESMDEONNMACH4OEMK2OWQUG76IRX2IQQHP2VYNSI7OHRAQ'
 
 mqtt = Mqtt(app)
 
@@ -43,20 +44,44 @@ def getLast():
     else:  
         return jsonify(store_data[len(store_data)-1]) 
 
+### Simulated ###
+# @mqtt.on_connect()
+# def handle_connect(client, userdata, flags, rc):
+#     # To removed here
+#     mqtt.subscribe('test')
+#     #mqtt.subscribe('#')
+
+# hour_counter = 1
+# @mqtt.on_message()
+# def handle_mqtt_message(client, userdata, message):
+#     data = dict(
+#         topic=message.topic,
+#         store=message.payload.decode()[10:15],
+#         value=int(message.payload.decode()[16:len(message.payload.decode())-1]),
+#         time=store_data[len(store_data)-1]['time'] +  datetime.timedelta(hours=1)
+#     )
+#     print("Appending")
+#     store_data.append(data)
+
+### TTN ###
 @mqtt.on_connect()
 def handle_connect(client, userdata, flags, rc):
     # To removed here
-    mqtt.subscribe('test')
+    mqtt.subscribe('v3/p1-05-mall-tracker@ttn/devices/eui-70b3d57ed004df25/up')
     #mqtt.subscribe('#')
 
 hour_counter = 1
 @mqtt.on_message()
 def handle_mqtt_message(client, userdata, message):
+    payload = json.loads(message.payload.decode())
     data = dict(
         topic=message.topic,
-        store=message.payload.decode()[10:15],
-        value=int(message.payload.decode()[16:len(message.payload.decode())-1]),
+        store=payload['uplink_message']['decoded_payload']['string'].split(",")[0],
+        value=payload['uplink_message']['decoded_payload']['string'].split(",")[1],
         time=store_data[len(store_data)-1]['time'] +  datetime.timedelta(hours=1)
     )
+    
+    # print(payload['uplink_message']['decoded_payload']['string'])
+
     print("Appending")
     store_data.append(data)
